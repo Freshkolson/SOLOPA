cfd4ecad7e3489158e39a9a4ef21a029ef82a2a7
Freshkolson <yaneponimayuvass@gmail.com>
2025-05-09 21:58:16 +0200

    Fix

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
 
+# Создаем приложение
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Запускаем приложение на порту 10000, так как Render требует его
+    app.run(host="0.0.0.0", port=10000)
